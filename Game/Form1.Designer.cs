89ccfac51132e5251720f2e319c166b83058735e
Eyyüp Meşe <Eyyp Mee@DESKTOP-2CN1H88>
2025-10-01 22:29:11 +0300

    Proje dosyası ekle.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.birdPictureBox = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.bottomPipe = new System.Windows.Forms.PictureBox();
+            this.topPipe = new System.Windows.Forms.PictureBox();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.birdPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomPipe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topPipe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // birdPictureBox
+            // 
+            this.birdPictureBox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.birdPictureBox.Location = new System.Drawing.Point(120, 213);
+            this.birdPictureBox.Name = "birdPictureBox";
+            this.birdPictureBox.Size = new System.Drawing.Size(50, 40);
+            this.birdPictureBox.TabIndex = 0;
+            this.birdPictureBox.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 25;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // bottomPipe
+            // 
+            this.bottomPipe.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.bottomPipe.Location = new System.Drawing.Point(500, 300);
+            this.bottomPipe.Name = "bottomPipe";
+            this.bottomPipe.Size = new System.Drawing.Size(80, 150);
+            this.bottomPipe.TabIndex = 1;
+            this.bottomPipe.TabStop = false;
+            // 
+            // topPipe
+            // 
+            this.topPipe.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.topPipe.Location = new System.Drawing.Point(500, 0);
+            this.topPipe.Name = "topPipe";
+            this.topPipe.Size = new System.Drawing.Size(80, 150);
+            this.topPipe.TabIndex = 2;
+            this.topPipe.TabStop = false;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(1038, 142);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(51, 16);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "score:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 451);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.topPipe);
+            this.Controls.Add(this.bottomPipe);
+            this.Controls.Add(this.birdPictureBox);
+            this.Location = new System.Drawing.Point(500, 0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.birdPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomPipe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topPipe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox birdPictureBox;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox bottomPipe;
+        private System.Windows.Forms.PictureBox topPipe;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        int gravity = 1;
+        int jump = -10;
+        int birdVelocity = 0;
+        int score = 0;
+        int pipeGap = 150;
+
+        bool isGameOver = false;
+        Random rnd = new Random();
+
+        // Hız kontrolü
+        float pipeSpeed = 3f;         // başlangıç hızı
+        float maxPipeSpeed = 20f;     // maksimum hız
+        float speedIncrement = 0.01f; // her tick artış miktarı
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (isGameOver)
+                {
+                    RestartGame();
+                }
+                else
+                {
+                    birdVelocity = jump;
+                }
+            }
+        }
+
+        private void RestartGame()
+        {
+            birdPictureBox.Top = 200;
+            birdVelocity = 0;
+
+            topPipe.Left = 500;
+            bottomPipe.Left = 500;
+
+            score = 0;
+            scoreLabel.Text = "Score: 0";
+
+            isGameOver = false;
+
+            pipeSpeed = 5f; // hız sıfırlanır
+
+            gameTimer.Start();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            // Kuş hareketi
+            birdPictureBox.Top += birdVelocity;
+            birdVelocity += gravity;
+
+            // Boru hızını artır, maxPipeSpeed'i geçme
+            if (pipeSpeed < maxPipeSpeed)
+            {
+                pipeSpeed += speedIncrement;
+            }
+
+            // Boruları sola kaydır
+            topPipe.Left -= (int)pipeSpeed;
+            bottomPipe.Left -= (int)pipeSpeed;
+
+            // Borular ekranın solundan çıkarsa tekrar sağa gönder ve rastgele yükseklik ayarla
+            if (topPipe.Right < 0)
+            {
+                int topHeight = rnd.Next(50, this.ClientSize.Height - pipeGap - 50);
+                topPipe.Height = topHeight;
+
+                bottomPipe.Top = topHeight + pipeGap;
+                bottomPipe.Height = this.ClientSize.Height - bottomPipe.Top;
+
+                topPipe.Left = 500;
+                bottomPipe.Left = 500;
+
+                score++;
+                scoreLabel.Text = "Score: " + score;
+            }
+
+            // Çarpışma ve ekran altına düşme kontrolü
+            if (birdPictureBox.Top + birdPictureBox.Height > this.ClientSize.Height ||
+                birdPictureBox.Bounds.IntersectsWith(topPipe.Bounds) ||
+                birdPictureBox.Bounds.IntersectsWith(bottomPipe.Bounds))
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Oyun Bitti!");
+                isGameOver = true;
+            }
+        }
+    }
+
+
+}
